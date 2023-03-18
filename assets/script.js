0db8b9f49f46403aed9ee50838ee8653c832a7b0
Stephanie Jauch <sjauch21@gmail.com>
2023-03-18 12:55:43 -0500

    temp by city

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,26 @@ var weatherEl = document.getElementById('weather');
 var forecastEl = document.getElementById('forecast');
 var historyEl = document.getElementById('history');
 var searchBtn = document.getElementById('search-button');
-var APIkey = '763656d325fd4ae7170f7ec265c5aef0';
+//apikey to make it easier
+var apiKey = '763656d325fd4ae7170f7ec265c5aef0';
 
 function getApi() {
     var searchValue = document.getElementById('search-city').value;
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${APIkey}&units=imperial`;
   
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        $('#search-city').val('')
+ //search temperature by city      
+        var temperature = document.createElement('div')
+        temperature.textContent = "Temperature: " + data.main.temperature + " F";
+        temperature.classList = "current-list-group";
+
+        var cityEl = document.createElement(h2);
+        cityEl.textContent = data.name;
+        
+        })
